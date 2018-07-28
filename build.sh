13ddbc2e72fcbda5c5eee906d0e2aa578c37b6c1
cupcake <jbw@users.noreply.github.com>
2018-07-28 15:32:38 +0100

    fix build

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ VERSION=${1}
 cd hadoop-$VERSION-src
 
 echo "Building Hadoop $VERSION"
-mvn package -e -X -Pdist,native -DskipTests=true -Dtar 
+mvn package -Pdist -DskipTests -Dtar 
 
 if [[ $? -eq 0 ]]; then
   echo -e "\n\nHadoop $VERSION build complete.\n\n"
